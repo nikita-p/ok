b4a71104840243260a265e66e3ba9d4295588f8c
nikita-p <nikita.petrov.1997@gmail.com>
2016-03-02 22:25:12 +0600

    Создан пустой элемент в списке

--- list.cpp
+++ list.cpp
@@ -24,8 +24,8 @@ class AbstractList
 {
 protected:
     T _default;
-public :
-    virtual void sort(bool (*f) (T* first, T* second)) = 0;
+public:
+    virtual void sort(bool(*f) (T* first, T* second)) = 0;
     virtual T get(int index) = 0;
     virtual void set(int index, T data) = 0;
     virtual void insert(int index, T data) = 0;
@@ -50,7 +50,7 @@ public :
 
     virtual ostream& print(ostream& o)
     {
-        for (int i = 0; i < len(); i ++)
+        for (int i = 0; i < len(); i++)
         {
             o << get(i) << endl;
         }
@@ -61,7 +61,7 @@ public :
     {
         int count;
         in >> count;
-        for (int i = 0 ; i < count ; i ++)
+        for (int i = 0; i < count; i++)
         {
             T tmp;
             in >> tmp;
@@ -82,12 +82,12 @@ protected:
 private:
     virtual void set(int index, T data, int c)
     {
-        if(index == c)
+        if (index == c)
             _data = data;
-        if(next == NULL)
+        if (next == NULL)
             return;
         else
-            next->set(index,data,c+1);
+            next->set(index, data, c + 1);
     }
     virtual void insert(T data)
     {
@@ -97,32 +97,32 @@ private:
     }
     virtual void insert(T data, int pos, int c)
     {
-        if(next==NULL)
+        if (next->next == NULL)
         {
-            list* b = new list(data, _default, NULL);
+            list* b = new list(data, _default, next);
             next = b;
             return;
         }
-        if(c == (pos-1))
+        if (c == (pos - 1))
         {
             list* b = new list(data, _default, next);
             next = b;
         }
         else
-            next->insert(data,pos,c+1);
+            next->insert(data, pos, c + 1);
     }
     virtual T _get(int index)
     {
-        if(index == 0)
+        if (index == 0)
             return _data;
-        if(next== NULL)
+        if (next == NULL)
             return _default;
         else
-            return next->get(index-1);
+            return next->get(index - 1);
     }
     virtual T remove(int pos, int i)
     {
-        if(pos==i+1)
+        if (pos == i + 1)
         {
             list* b = next->next;
             T s = next->_data;
@@ -131,20 +131,20 @@ private:
             return s;
         }
         else
-            next->remove(pos, i+1);
+            next->remove(pos, i + 1);
     }
     virtual int len(int c)
     {
-        if(next == NULL)
-            return (c+1);
+        if (next == NULL)
+            return (c + 1);
         else
-            return next->len(c+1);
+            return next->len(c + 1);
     }
-    virtual void sort_line(bool (*f) (T*, T*))
+    virtual void sort_line(bool(*f) (T*, T*))
     {
-        if(next->next==NULL)
+        if (next->next->next == NULL)
             return;
-        if(f(&(next->_data), &(next->next->_data)))
+        if (f(&(next->_data), &(next->next->_data)))
         {
             list* tmp = next;
             next = next->next;
@@ -156,8 +156,8 @@ private:
 public:
     list()
     {
-        _data=0;
-        next=NULL;
+        _data = 0;
+        next = NULL;
     }
     list(T data, T def)
     {
@@ -177,20 +177,20 @@ public:
     }
     virtual void insert(int index, T data)
     {
-        if(index==0)
+        if (index == 0 || len()==0)
             insert(data);
         else
             insert(data, index, 0);
     }
-    virtual T get (int index)
+    virtual T get(int index)
     {
         return _get(index);
     }
     virtual T remove(int index)
     {
-        if(next == NULL)
+        if (next == NULL)
             return _default;
-        if(index==0)
+        if (index == 0)
         {
             list* b = next;
             T s = _data;
@@ -203,12 +203,12 @@ public:
     }
     virtual int len()
     {
-        return len(0);
+        return len(0)-1;
     }
-    virtual void sort(bool (*f) (T*, T*))
+    virtual void sort(bool(*f) (T*, T*))
     {
         insert(0, _default);
-        for(int i=0;i<len(); i++)
+        for (int i = 0; i<len(); i++)
             sort_line(f);
         remove(0);
     }
@@ -217,32 +217,13 @@ public:
 template <typename T>
 bool gr(T* first, T* second)
 {
-    if(*first > *second)
+    if (*first > *second)
         return true;
     return false;
 }
 
 AbstractList<string>* get_init()
 {
-    string e("EMPTY!");
-    string d("Hello world!");
-    list<string>* s = new list<string>(e, d);
+    list<string>* s = new list<string>("EMPTY", "!");
     return s;
 }
-
-/*
-int main()
-{
-    AbstractList<string>* al = get_init();
-    al->read(cin);
-    al->print(cout);
-        al->insert(4,"dda");
-        al->insert(4,"ddf");
-        al->insert(4,"dsd");
-        al->insert(4,"edd");
-        al->print(cout);
-        al->sort(gr);
-        al->print(cout);
-    return 0;
-}
-*/
